9d11ca08d4c6d163f5f75df561f3642c053d8d22
Iris Monstad <irismonstad@gmail.com>
2026-03-24 14:09:23 +0100

    Laget ruter og skjema for å opprette bruker

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const PORT = 3000;
 const cors = require('cors');
 app.use(cors());
 
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
+
 const Database = require('better-sqlite3');
 const db = new Database('studentbolig.db');
 
@@ -17,4 +20,26 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Serveren kjører på http://localhost:${PORT}`);
+});
+
+app.post('/lagBruker', (req, res) => {
+    try {
+        let {mail, mobil, fornavn, etternavn, passord} = req.body;
+        mail = mail.toString().trim();
+        mobil = mobil.toString().trim();
+        fornavn = fornavn.toString().trim();
+        etternavn = etternavn.toString().trim();
+        passord = passord.toString().trim();
+
+        console.log('Laget bruker ', fornavn, etternavn);
+
+        db.prepare('INSERT INTO bruker (mail, mobil, fornavn, etternavn, passord) VALUES (?, ?, ?, ?, ?)').run(mail, mobil, fornavn, etternavn, passord);
+
+        return res.sendStatus(201);
+    }
+
+    catch (err) {
+        console.error('Feil ved opprettelse av bruker');
+        return res.status(500).json({error: 'Kunne ikke lage bruker'});
+    }
 });
