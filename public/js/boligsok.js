718c383c5c40d56b3001fb30f275e49697aa5035
Iris Monstad <irismonstad@gmail.com>
2026-04-07 14:40:06 +0200

    Lagt til midlertidig funksjonalitet som lagrer adresse fra alle søk i databasen

--- public/js/boligsok.js
+++ public/js/boligsok.js
@@ -4,9 +4,19 @@ skjema.addEventListener("submit", sokAdresse);
 const adresseEl = document.querySelector("#adresse");
 async function sokAdresse(e) {
     e.preventDefault();
-    adresse = adresseEl.value.trim();
-    const resultat = await fetch(`https://ws.geonorge.no/adresser/v1/sok?sok=${adresse}`);
+    const adresseSok = adresseEl.value.trim();
+    const resultat = await fetch(`https://ws.geonorge.no/adresser/v1/sok?sok=${adresseSok}`);
     const data = await resultat.json();
+
+    const adresse = data.adresser[0].adressetekst;
+    const breddegrad = data.adresser[0].representasjonspunkt.lat;
+    const lengdegrad = data.adresser[0].representasjonspunkt.lon;
+
+    const res = await fetch('/lagAdresse', {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({adresse, breddegrad, lengdegrad})
+    });
 
     console.log(data.adresser[0]);
 }
