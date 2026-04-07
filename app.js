8943b2578222ff4ba0298ce73419b0987445ced0
Iris Monstad <irismonstad@gmail.com>
2026-04-07 14:20:48 +0200

    Laget rute for å lagre adresse i databasen, endret format i databasen til (id, adresse, breddegrad, lengdegrad)

--- app.js
+++ app.js
@@ -43,3 +43,22 @@ app.post('/lagBruker', (req, res) => {
         return res.status(500).json({error: 'Kunne ikke lage bruker'});
     }
 });
+
+app.post('/lagAdresse', (req, res) => {
+    try {
+        let {adresse, breddegrad, lengdegrad} = req.body;
+        adresse = adresse.toString().trim();
+        breddegrad = breddegrad.toString().trim();
+        lengdegrad = lengdegrad.toString.trim();
+
+        console.log(`Opprettet adresse ${adresse} ved ${breddegrad}, ${lengdegrad}`)
+
+        db.prepare('INSERT INTO adresse (adresse, breddegrad, lengdegrad) VALUES (?, ?, ?)').run(adresse, breddegrad, lengdegrad);
+        return res.sendStatus(201);
+    }
+    catch(err) {
+        console.error('Feil ved opprettelse av adresse');
+        return res.status(500).json({error: 'Kunne ikke opprette adresse'});
+    }
+})
+
