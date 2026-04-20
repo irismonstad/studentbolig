a96e018789b112a7f0283030d00254d8d841e587
Iris Monstad <irismonstad@gmail.com>
2026-04-20 22:07:35 +0200

    Laget en global navigasjonsbar

---
+++ public/js/navbar.js
@@ -0,0 +1,13 @@
+const navHTML = 
+`<nav class="globalnav">
+    <ul class="lenker">
+        <li><a href="/">Hjem</a></li>
+        <li><a href="/html/boligsok.html">Søk</a></li>
+        <li><a href="/html/profil.html">Profil</a></li>
+        <li><a href="/html/lagret.html">Lagret</a></li>
+        <li><a href="/html/login.html">Logg inn</a></li>
+    </ul>
+</nav>`;
+
+document.body.insertAdjacentHTML('afterbegin', navHTML);
+
