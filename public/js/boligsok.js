f66523de378c3464c05e68115c235ac09c86521c
Iris Monstad <irismonstad@gmail.com>
2026-04-20 21:06:42 +0200

    Lagt til funksjonalitet for å lagre adresse til brukers favoritter

--- public/js/boligsok.js
+++ public/js/boligsok.js
@@ -38,7 +38,7 @@ async function sokAdresse(e) {
 
 async function lagreAdresse() {
     // Legger adresser fra søk i databasen
-    const res = await fetch('/api/lagAdresse', {
+    const res = await fetch('/api/lagreAdresse', {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify({adresse, breddegrad, lengdegrad})
--- routes/adresse.js
+++ routes/adresse.js
@@ -2,26 +2,33 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.post('/lagAdresse', (req, res) => {
+router.post('/lagreAdresse', (req, res) => {
+    if (!req.session.brukerId) return res.status(401).send("Ikke innlogget");
+
+    const brukerId = req.session.brukerId;
+
     try {
         let {adresse, breddegrad, lengdegrad} = req.body;
         adresse = adresse.toString().trim();
         breddegrad = breddegrad.toString().trim();
         lengdegrad = lengdegrad.toString().trim();
 
-        console.log(`Opprettet adresse ${adresse} ved ${breddegrad}, ${lengdegrad}`)
+        // INSERT OR IGNORE INTO legger raden inn i databasen kun hvis den ikke finnes enda. 
+        db.prepare('INSERT OR IGNORE INTO adresse (adresse, breddegrad, lengdegrad) VALUES (?, ?, ?)').run(adresse, breddegrad, lengdegrad);
 
-        db.prepare('INSERT INTO adresse (adresse, breddegrad, lengdegrad) VALUES (?, ?, ?)').run(adresse, breddegrad, lengdegrad);
+        // Akkurat denne burde endres slik at adresser med samme navn skilles fra hverandre.
+        const adresseId = db.prepare('SELECT id FROM adresse WHERE adresse = ?').get(adresse).id;
+        
+        db.prepare('INSERT OR IGNORE INTO adresse_bruker (bruker_id, adresse_id) VALUES (?,?)').run(brukerId, adresseId);
+
+        console.log(`Lagret adresse ${adresse} ved ${breddegrad}, ${lengdegrad} brukerId ${brukerId}`);
         return res.sendStatus(201);
     }
     catch(err) {
-        console.error('Feil ved opprettelse av adresse');
+        console.error('Feil ved lagring av adresse');
         return res.status(500).json({error: 'Kunne ikke opprette adresse'});
     }
 });
-
-
-
 
 router.get('/hentAdresser', (req, res) => {
     const rows = db.prepare(`
